6b2d2b96e2ecef158b4b4b5e99a5b6735b749820
Adam Crownoble <adam.crownoble@biola.edu>
2014-10-07 13:51:09 -0700

    People searching

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,6 +1,25 @@
 class PeopleController < ApplicationController
   def index
-    @people = Person.all.asc(:last_name, :preferred_name).page(params[:page]).per(60)
+    @people = Person.all
+    if params[:q]
+      begin
+        regex = Regexp.new(params[:q].gsub(/\s/, '.*'), Regexp::IGNORECASE)
+        @people = @people.any_of(
+          {first_name: regex},
+          {preferred_name: regex},
+          {last_name: regex},
+          {display_name: regex},
+          {title: regex},
+          {department: regex},
+          {residence: regex},
+          {'emails.address' => regex},
+          {'ids.identifier' => regex}
+        )
+      rescue RegexpError
+        @people = Person.none
+      end
+    end
+    @people = @people.asc(:last_name, :preferred_name).page(params[:page]).per(60)
   end
 
   def show
