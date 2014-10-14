952b1db9b843d3beb0edded2c844a633061457fb
Adam Crownoble <adam.crownoble@biola.edu>
2014-10-14 13:33:59 -0700

    Use mongodb session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,2 @@
-# Be sure to restart your server when you modify this file.
-
-Rails.application.config.session_store :cookie_store, key: '_three-keepers_session'
+require 'rack-cas/session_store/rails/mongoid'
+ThreeKeepers::Application.config.session_store :rack_cas_mongoid_store, key: '_three_keepers_session'
