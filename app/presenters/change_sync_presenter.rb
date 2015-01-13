f98931037f5ef55afd6de607f89211bf6c44c708
Adam Crownoble <adam.crownoble@biola.edu>
2015-01-13 14:46:35 -0800

    Order sync_logs descending by started_at time

--- app/presenters/change_sync_presenter.rb
+++ app/presenters/change_sync_presenter.rb
@@ -2,6 +2,6 @@ class ChangeSyncPresenter < ApplicationPresenter
   presents ChangeSync
 
   def sync_logs
-    SyncLogPresenter.map(model.sync_logs)
+    SyncLogPresenter.map(model.sync_logs.desc(:started_at))
   end
 end
