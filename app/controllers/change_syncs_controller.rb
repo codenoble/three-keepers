aaaf674dea40d874d3f5f95400ac41230901b278
Michael Stephens <michael.stephens@biola.edu>
2015-08-21 15:07:58 -0700

    Rerunning errored should reflect that

--- app/controllers/change_syncs_controller.rb
+++ app/controllers/change_syncs_controller.rb
@@ -2,7 +2,7 @@ class ChangeSyncsController < ApplicationController
   def update
     @changeset = Changeset.find(params[:changeset_id])
     @change_sync = @changeset.change_syncs.find(params[:id])
-    sync_log = @change_sync.sync_logs.find_by(:succeeded_at.ne => nil)
+    sync_log = @change_sync.sync_logs.find_by(:succeeded_at.ne => nil) unless params[:error]
 
     sync_log.message = "#{sync_log.message}\nReran by #{current_user.username} on #{Time.zone.now}.".strip
     @change_sync.run_after = Time.zone.now
