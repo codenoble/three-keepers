50288b9fe75376d9ea84b0052df333decccc62d2
Adam Crownoble <adam@obledesign.com>
2015-10-06 11:42:30 +0800

    Use menu block for main menu
    Allows views in multiple email related controllers to still highlight the
    Emails menu link.

--- app/helpers/link_helper.rb
+++ app/helpers/link_helper.rb
@@ -1,24 +1,4 @@
 module LinkHelper
-  # Set class on active navigation items
-  def nav_link(text, path_helper)
-    # The router can't match against a relative root in the path
-    # so we need to strip it out with script_name: nil.
-    router_path = send(path_helper, script_name: nil)
-    real_path = send(path_helper)
-
-    recognized = Rails.application.routes.recognize_path(router_path)
-
-    if recognized[:controller] == params[:controller] # && recognized[:action] == params[:action]
-      content_tag(:li, :class => "descendant active") do
-        link_to( text, real_path)
-      end
-    else
-      content_tag(:li, :class => "descendant") do
-        link_to( text, real_path)
-      end
-    end
-  end
-
   def menu_block(html_options = {}, &block)
     MenuBlock.new(self, html_options).render(&block)
   end
