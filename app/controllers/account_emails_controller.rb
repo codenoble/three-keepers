38f453f2822075512084b4d2952900f64be9796c
Adam Crownoble <adam@obledesign.com>
2015-12-18 10:40:42 +0800

    Exclude deleted emails in account_emails#search
    This API is only used for creating aliases at the moment and you
    shouldn't be able to create an alias for a deleted account.

--- app/controllers/account_emails_controller.rb
+++ app/controllers/account_emails_controller.rb
@@ -3,8 +3,10 @@ class AccountEmailsController < ApplicationController
     response = GoogleSyncinator::APIClient::AccountEmails.new.search(q: params[:term]).perform
 
     addresses = response.parse.map do |account_email|
-      {label: account_email['address'], value: account_email['id']}
-    end
+      if account_email['state'] != 'deleted'
+        {label: account_email['address'], value: account_email['id']}
+      end
+    end.compact
 
     render json: addresses.to_json, callback: params[:jsonCallback]
   end
